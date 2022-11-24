016730c7c11ed101345ba7b95806bc746063f080
Magnus Larsen <mal@it-minds.dk>
2022-11-24 16:25:51 +0100

    Refactored code RMA-132913
    Parameter
    
    
    Spacing
    
    
    Naming
    
    
    dawd

--- GitsterJam/Services/WeatherService.cs
+++ GitsterJam/Services/WeatherService.cs
@@ -2,25 +2,32 @@
 {
     public interface IWeatherService
     {
-        Task<IEnumerable<WeatherForecast>> GetForecast();
+        Task<IEnumerable<WeatherForecast>> GetForecast(int count);
     }
 
     public class WeatherService : IWeatherService
     {
         private static readonly string[] Summaries = new[]
         {
+
+
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public async Task<IEnumerable<WeatherForecast>> GetForecast()
+        public async Task<IEnumerable<WeatherForecast>> GetForecast(int amount)
         {
-            return await Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-           .ToArray());
+
+
+            var summaries = Enumerable.Range(1, amount)
+                .Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 45),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+            return await Task.FromResult(summaries);
         }
     }
 }
